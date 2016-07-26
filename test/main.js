46ab0144ca62b8c22e5671802cb0006d15bc509d
spearmootz <spear.ricardo@gmail.com>
2016-07-26 11:00:07 -0400

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,17 +1,17 @@
 // test/main.js
-var should = require('should');
+var assert = require('chai').assert;
 var nodeExcel = require('../index');
 
 
 describe('Simple Excel xlsx Export', function() {
     describe('Export', function() {
-        it('returns xlsx', function(done) {
+        it('returns path', function(done) {
 			var conf ={};
 			conf.cols = [
 				{caption:'string', type:'string'},
 				{caption:'date', type:'date'},
 				{caption:'bool', type:'bool'},
-				{caption:'number 2', type:'number'}				
+				{caption:'number 2', type:'number'}
 			];
 			conf.rows = [
         ['pi', (new Date(Date.UTC(2013, 4, 1))).oaDate(), true, 3.14],
@@ -19,12 +19,14 @@ describe('Simple Excel xlsx Export', function() {
         ["M&M<>'", (new Date(Date.UTC(2013, 6, 9))).oaDate(), false, 1.2],
         ["null", null, null, null]
 			];
-			
-        nodeExcel.execute(conf, function(err, result) {
+
+        nodeExcel.execute(conf, function(err, path) {
             if (err) throw err;
             var fs = require('fs');
-            fs.writeFileSync('d.xlsx', result, 'binary');
-            done(err);
+            fs.exists(path, (exists) => {
+                assert(exists, "file does not exist");
+                done();
+            });
 	    });
         });
     });
